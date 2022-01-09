09bc0235bab9d1214539b60f072baad4235874bf
Kasir Barati <node.js.developers.kh@gmail.com>
2022-01-09 19:52:08 +0330

    update: use env in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 import csurf from 'csurf';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 // All general configs for RESTful API
 import { corsConfigsGenerator } from './configs/cors.config';
 import { helmetConfigsGenerator } from './configs/helmet.config';
+import { webAppConfigs } from './contracts/types/web.type';
 
 // All Middleware
 import { csrfMiddlewareError } from './commons/middlewares/errors/csrf.middleware';
 import { csrfMiddleware } from './commons/middlewares/generals/csrf.middleware';
 
-import cookieParser from 'cookie-parser';
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const { corsConfigs } = corsConfigsGenerator();
     const { helmetConfigs } = helmetConfigsGenerator();
+    const configService = app.get(ConfigService);
+    const appConfigs =
+        configService.get<webAppConfigs>('webAppConfigs');
 
     app.use(cookieParser());
     app.use(
@@ -30,6 +34,7 @@ async function bootstrap() {
     app.use(csrfMiddlewareError);
     app.enableCors(corsConfigs);
     app.use(helmet(helmetConfigs));
-    await app.listen(3000);
+
+    await app.listen(appConfigs.port);
 }
 bootstrap();
